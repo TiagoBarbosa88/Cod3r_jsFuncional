c22b3340234bd748a2f8ee7b08d2e70f94df6993
TiagoBarbosa88 <sombr43@gmail.com>
2022-11-23 06:54:28 -0300

    callback

---
+++ Exercicios/funcao-somar/script.js
@@ -0,0 +1,12 @@
+function calcular() {
+  const contas = document.querySelector('#contas').value;
+  const contas1 = document.querySelector('#contas1').value;
+  const contas2 = document.querySelector('#contas2').value;
+  const btnCalcular = document.querySelector('#calcular');
+
+  const result = parseInt(contas) + parseInt(contas1) + parseInt(contas2) || parseInt(contas) + parseInt(contas1) || parseInt(contas) + parseInt(contas2) || parseInt(contas1) + parseInt(contas2);
+
+  document.querySelector('#resultado').innerHTML = `$ ${result}`;
+
+
+}
