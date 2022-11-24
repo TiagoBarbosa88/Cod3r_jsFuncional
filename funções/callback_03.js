519a43831fdcddbae6c0367afbb2768f4f778b7a
TiagoBarbosa88 <sombr43@gmail.com>
2022-11-23 22:13:12 -0300

    callback map

---
+++ funções/callback_03.js
@@ -0,0 +1,45 @@
+const nums = [1,2,3,4,5]
+const dobro = n => n * 2
+
+//console.log(nums.map(dobro));
+
+const nomes = ["Ana", "Bia", "Lia", "Gui", "Rafa"]
+const primeiraLetra = texto => texto[0]
+
+
+//console.log(nomes.map(primeiraLetra));
+
+const carrinho = [
+  {nome: "Caneta", qtde: 10, preco: 7.99},
+  {nome: "Impressora", qtde: 0, preco: 649.50},
+  {nome: "Caderno", qtde: 4, preco: 27.10},
+  {nome: "Lapis", qtde: 3, preco: 5.82},
+  {nome: "Tesoura", qtde: 1, preco: 19.20},
+]
+
+
+/* 
+
+const getNome = item => item.nome
+console.log(carrinho.map(getNome));
+
+const somar = item => item.qtde * item.preco
+const totais = carrinho.map(somar)
+console.log(totais); 
+
+*/
+
+Array.prototype.meuMap = function(fn){
+  const novoArray = []
+  for(let i=0; i < this.length; i++){
+    novoArray.push(fn(this[i], i, this))
+  }
+  return novoArray
+}
+
+const getNome = item => item.nome
+console.log(carrinho.meuMap(getNome));
+
+const somar = item => item.qtde * item.preco
+const totais = carrinho.meuMap(somar)
+console.log(totais);
