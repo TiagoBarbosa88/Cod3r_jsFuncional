e22dd8e5d39cab8123c67821bba6d072b5b01145
TiagoBarbosa88 <sombr43@gmail.com>
2022-11-27 14:48:10 -0300

    desafio 2 filter, map, reduce

---
+++ funções/desafio_2.js
@@ -0,0 +1,51 @@
+const carrinho = [
+  {nome: "Caneta", qtde: 10, preco: 7.99, fragil: true},
+  {nome: "Impressora", qtde: 1, preco: 649.50, fragil: true},
+  {nome: "Caderno", qtde: 4, preco: 27.10, fragil: false},
+  {nome: "Lapis", qtde: 3, preco: 5.82, fragil: false},
+  {nome: "Tesoura", qtde: 1, preco: 19.20, fragil: true},
+]
+
+
+// Desafio filter, map, reduce
+
+const media = carrinho
+      .filter(item => item.fragil)
+      .map(item => item.qtde * item.preco)
+      .reduce((acc, el) =>{
+        const novaQtde = acc.qtde + 1
+        const novoTotal = acc.total + el
+        console.log(acc, el);
+        return {
+          qtde: novaQtde, 
+          total: novoTotal, 
+          media: novoTotal / novaQtde
+        }
+
+      }, {qtde: 0, total: 0, media: 0 })
+      .media
+
+console.log(`A média é ${media}`);      
+
+
+
+
+
+
+/* 
+
+// 1. fragil true
+const frageis = item => item.fragil === true
+const itensFragil = carrinho.filter(frageis)
+console.log(itensFragil);
+
+// 2 . qdt * preco -> total
+const getTotal = (item) => item.qtde * item.preco
+const totais = carrinho.map(getTotal)
+console.log(totais);
+
+// 3. media totais
+const totalGeral = carrinho.reduce(acc, totais)
+console.log(totalGeral); 
+
+*/
